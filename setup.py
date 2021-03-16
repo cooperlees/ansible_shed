196a4d6242a8cd1a05dbc15f29e25a350b5ba4d8
Cooper Lees <me@cooperlees.com>
2021-03-15 20:32:54 -0700

    Change version due to mooch's contribution

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ ptr_params = {
 
 setup(
     name="ansible_shed",
-    version="2021.3.10",
+    version="2021.3.15",
     description=(
         "asyncio ansible tower like shed to run playbooks and have prometheus "
         + "collector stats"
