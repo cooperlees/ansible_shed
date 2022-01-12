1471d3b74153c31e0f22668836338c57a5ddfda3
Chris Miceli <github@miceli.net.au>
2022-01-12 15:34:39 +1100

    make the bind address configurable (#12)
    * make the bind address configurable
    
    * bumping version
    
    * Update ansible_shed/shed.py
    
    Use get default for dict rather than or
    
    Co-authored-by: Chris Miceli <chris@miceli.net.au>
    Co-authored-by: Cooper Lees <me@cooperlees.com>

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ ptr_params = {
 
 setup(
     name="ansible_shed",
-    version="2021.4.15",
+    version="2022.1.5",
     description=(
         "asyncio ansible tower like shed to run playbooks and have prometheus "
         + "collector stats"
